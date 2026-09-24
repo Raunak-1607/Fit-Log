2784defdf2ddc1f9c5645ac26525de861e422771
Raunak-1607 <raunakbiswas537@gmail.com>
2026-09-24 22:49:47 +0600

    Updated Navbar

--- src/app/Components/Shared-Components/Navbar.tsx
+++ src/app/Components/Shared-Components/Navbar.tsx
@@ -18,73 +18,70 @@ const Navbar = () => {
       <li>
         <Link href="/Components/My-Plan">My Plan</Link>
       </li>
-
-      
     </>
   );
   return (
     <div className="navbar bg-base-100 shadow-sm">
-
-    <div className="navbar bg-base-100 shadow-sm container mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              aria-label="Menu"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      <div className="navbar bg-base-100 shadow-sm container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                aria-label="Menu"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
+          <Link href="/" className="btn btn-ghost text-xl">
+            <Image
+              src={logo}
+              alt="Fit Log"
+              width={35}
+              height={10}
+              className="object-contain"
+            />
+            <span className="bold">FITLOG</span>
+          </Link>
         </div>
-        <a className="btn btn-ghost text-xl">
-          <Image
-            src={logo}
-            alt="Fit Log"
-            width={35}
-            height={10}
-            className="object-contain"
-          />
-          <span className="bold">FITLOG</span>
-        </a>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          {/* <a className="btn">Button</a> */}
+          <Link href="/Components/My-Plan " className="flex gap-2">
+            {" "}
+            <span>Plan</span>
+            <span className=" w-8 rounded-full bg-[#ccff00] text-black flex items-center justify-center font-bold">
+              {addPlan.length}
+            </span>
+          </Link>
+          <Link href="/Components/My-Plan " className="flex gap-2">
+            {" "}
+            <span>Saved</span>
+            <span className=" w-8 rounded-full border-gray-500 border-1 text-white flex items-center justify-center font-bold">
+              {savePlan.length}
+            </span>
+          </Link>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end flex gap-4">
-        {/* <a className="btn">Button</a> */}
-        <Link href="/Components/My-Plan " className="flex gap-2">
-          {" "}
-          <span>Plan</span>
-          <span className=" w-8 rounded-full bg-[#ccff00] text-black flex items-center justify-center font-bold">
-            {addPlan.length}
-          </span>
-        </Link>
-        <Link href="/Components/My-Plan " className="flex gap-2">
-          {" "}
-          <span>Saved</span>
-          <span className=" w-8 rounded-full border-gray-500 border-1 text-white flex items-center justify-center font-bold">
-            {savePlan.length}
-          </span>
-        </Link>
-      </div>
-    </div>
     </div>
   );
 };
